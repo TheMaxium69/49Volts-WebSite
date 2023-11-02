3d7542fe38ba7ca72c794af12c8de8871328af5e
Nueve69 <yder09@gmail.com>
2023-11-02 16:01:42 +0100

    maj pages

---
+++ collaboration.php
@@ -0,0 +1,6 @@
+<?php include "app/app.php"; $page = 1; head($page); ?>
+<body> <header> <?php navbar($page); ?> </header>
+
+	
+
+</body> </html>
---
+++ prestation.php
@@ -0,0 +1,27 @@
+<?php include "app/app.php"; $page = 1; head($page); ?>
+<body> <header> <?php navbar($page); ?> </header>
+
+<section id="service">
+            <h2 class="text-center m-5">Prestations de services</h2>
+            <div class="container">
+                <div class="card">
+                    <div class="content">
+                        <div class="h6">MONTAGE VIDEO</div>
+                        <div class="hover_content">
+                            <p>mParticle’s customer data platform empowers you to Integrate all of your data and orchestrate it across channels, partners, and systems.</p>
+                        </div>
+                    </div>
+                </div>     
+                
+                <div class="card">
+                    <div class="content">
+                        <div class="h6">CREATION LOGO</div>
+                        <div class="hover_content">
+                            <p>mParticle’s customer data platform empowers you to Integrate all of your data and orchestrate it across channels, partners, and systems.</p>
+                        </div>
+                    </div>
+                </div> 
+            </div>
+          </section>	
+
+</body> </html>
