9216e0530c8e74a233bf99b059b0f6e7d375e73a
Nueve69 <yder09@gmail.com>
2023-11-07 12:12:06 +0100

    prestation

--- prestation.php
+++ prestation.php
@@ -24,6 +24,163 @@
             </div>
           </section>	
 
+          		<!-- Features -->
+		<section id="features" class="bg-grey">
+			
+			<!-- Container -->
+			<div class="container">
+				
+				<!-- Section title -->
+				<div class="row justify-content-center">
+					<div class="col-12 col-md-10 col-lg-6">
+						
+						<div class="section-title text-center">
+							<h3>Awesome Features</h3>
+							<p>Sed laoreet diam sagittis tempus convallis. Interdum et malesuada fames ac ante ipsum primis in faucibus. </p>
+						</div>
+						
+					</div>
+				</div>
+			
+				<div class="row d-flex align-items-center">
+					
+					<!-- Left -->
+					<div class="col-12 col-md-6 col-lg-4"> 				
+						
+						<ul class="features-item">
+							
+							<!-- Feature box -->
+							<li> 
+								<div class="feature-box box-left d-flex"> 
+									
+									<!-- Box icon -->
+									<div class="box-icon">
+										<div class="icon icon-basic-gear"></div>
+									</div>
+									
+									<!-- Box Text -->
+									<div class="box-text align-self-center align-self-md-start">
+										<h4>Custom Shortcuts</h4>
+										<p>Semper a augue suscipit, luctus neque purus ipsum neque dolor primis libero tempus velna culpa expedita.</p>
+									</div>
+									
+								</div>
+							</li>
+							
+							<!-- Feature box -->
+							<li> 
+								<div class="feature-box box-left d-flex"> 
+									
+									<!-- Box icon -->
+									<div class="box-icon">
+										<div class="icon icon-basic-lock"></div>
+									</div>
+									
+									<!-- Box Text -->
+									<div class="box-text align-self-center align-self-md-start">
+										<h4>Secure Integration</h4>
+										<p>Semper a augue suscipit, luctus neque purus ipsum neque dolor primis libero tempus velna culpa expedita.</p>
+									</div>
+									
+								</div>
+							</li>
+							
+							<!-- Feature box -->
+							<li> 
+								<div class="feature-box box-left d-flex"> 
+									
+									<!-- Box icon -->
+									<div class="box-icon">
+										<div class="icon icon-basic-message-txt"></div>
+									</div>
+									
+									<!-- Box Text -->
+									<div class="box-text align-self-center align-self-md-start">
+										<h4>Free Live Chat</h4>
+										<p>Semper a augue suscipit, luctus neque purus ipsum neque dolor primis libero tempus velna culpa expedita.</p>
+									</div>
+									
+								</div>
+							</li>
+							
+						</ul>
+					</div>
+					
+					<!-- Center -->
+					<div class="col-12 col-lg-4 d-none d-lg-block"> 
+						<div class="features-thumb text-center">
+							<img src="images/features/awesome-features.png" alt="" />
+						</div>
+					</div>
+					
+					<!-- Right -->
+					<div class="col-12 col-md-6 col-lg-4"> 
+						
+						<ul class="features-item">
+							
+							<!-- Feature box -->
+							<li> 
+								<div class="feature-box d-flex"> 
+									
+									<!-- Box icon -->
+									<div class="box-icon">
+										<div class="icon icon-basic-share"></div>
+									</div>
+									
+									<!-- Box Text -->
+									<div class="box-text align-self-center align-self-md-start">
+										<h4>Social Share</h4>
+										<p>Semper a augue suscipit, luctus neque purus ipsum neque dolor primis libero tempus velna culpa expedita.</p>
+									</div>
+									
+								</div>
+							</li>
+							
+							<!-- Feature box -->
+							<li> 
+								<div class="feature-box d-flex"> 
+									
+									<!-- Box icon -->
+									<div class="box-icon">
+										<div class="icon icon-basic-sheet-multiple"></div>
+									</div>
+									
+									<!-- Box Text -->
+									<div class="box-text align-self-center align-self-md-start">
+										<h4>Merge Files</h4>
+										<p>Semper a augue suscipit, luctus neque purus ipsum neque dolor primis libero tempus velna culpa expedita.</p>
+									</div>
+									
+								</div>
+							</li>
+							
+							<!-- Feature box -->
+							<li> 
+								<div class="feature-box d-flex"> 
+									
+									<!-- Box icon -->
+									<div class="box-icon">
+										<div class="icon icon-basic-alarm"></div>
+									</div>
+									
+									<!-- Box Text -->
+									<div class="box-text align-self-center align-self-md-start">
+										<h4>Action Reminder</h4>
+										<p>Semper a augue suscipit, luctus neque purus ipsum neque dolor primis libero tempus velna culpa expedita.</p>
+									</div>
+									
+								</div>
+							</li>
+							
+						</ul>
+					</div>
+					
+				</div>
+				
+			</div>
+			
+		</section>
+
 
           
           
